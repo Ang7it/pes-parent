05bf45087ca48d360b133aacea11ff72e9c37e4e
Angshit Ghosh <angshitghosh@gmail.com>
2023-07-25 21:27:42 +0530

    Initial commit

---
+++ src/main/java/com/pespurse/PesParentApplication.java
@@ -0,0 +1,13 @@
+package com.pespurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pespurse/PesParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pespurse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
